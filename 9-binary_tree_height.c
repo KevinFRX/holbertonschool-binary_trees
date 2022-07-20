b1d50ad45738d5f58edc309e862f526b415711f8
KevinFRX <xerokf13@gmail.com>
2022-07-19 22:41:07 -0700

    1

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -10,5 +10,18 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+	size_t l = 0, r = 0;
 
+	if (!tree)
+		return (0);
+
+	if (tree->left)
+		l = binary_tree_height(tree->left) + 1;
+	if (tree->right)
+		r = binary_tree_height(tree->right) + 1;
+
+	if (l >= r)
+		return (l);
+	else
+		return (r);
 }
